2f5f19b729f1f379a8b6ded40c85963d33fd7bee
Ckey230 <Ckey230@gmail.com>
2022-12-01 20:23:04 -0500

    create and new routes complete

--- controllers/bread.js
+++ controllers/bread.js
@@ -6,16 +6,35 @@ router.get('/', (req,res) => {
     res.render('index', {
         breads: Bread
     })
-    //res.send(Bread)
+   
+})
+
+// Get: create new bread page 
+router.get('/new', (req,res) => {
+    res.render('new')
 })
 
 //Get: get a bread by its index
-// : = query paramter
+// : = query parameter
 router.get('/:index', (req,res) => {
     const { index } = req.params
     res.render('show', {
         bread: Bread[index]
     })
+
+})
+
+//Post: create a bread
+router.post('/', (req,res) => {
+    const { hasGluten, image } = req.body
+    if(!image) req.body.image = 'https://suebeehomemaker.com/wp-content/uploads/2021/10/sliced-french-bread.jpg'
+    if (hasGluten === 'on'){
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
 
 })
 
